49ef4099e81df4581d8ab482e672956c224ce431
duonglh113 <lehoangduong126@gmai.com>
2024-01-21 16:57:00 +0700

    slide 92 chuong 2

---
+++ baithuchanhJava/phuongtrinhbac2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class phuongtrinhbac2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Nhập b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Nhập c: ");
+        double c = scanner.nextDouble();
+
+        double delta = b * b - 4 * a * c;
+
+        if (delta > 0) {
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Nghiệm của phương trình là: x1 = " + x1 + ", x2 = " + x2);
+        } else if (delta == 0) {
+            double x = -b / (2 * a);
+            System.out.println("Nghiệm kép của phương trình là: x = " + x);
+        } else {
+            System.out.println("Phương trình vô nghiệm");
+        }
+
+        scanner.close();
+    }
+}
