2ed336f5d4e4f30c6a442e9d21f71e4d63d987f6
duonglh113 <lehoangduong126@gmai.com>
2024-03-12 21:29:28 +0700

    thuc hanh chuong 3

---
+++ lap2/slide95bai6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Slide95bai6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println(" Nhap vao so nguyen duong bat ky ");
+        n = sc.nextInt();
+        int giaiThua = 1;
+        for (int i = n; i > 0; i--) {
+            giaiThua *= i;
+        }
+        System.out.println(giaiThua);
+    }
+}
